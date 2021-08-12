ffc437b6281f736d8c922fdc842d24cefbae8e00
Thang Vo <thangvo406@gmail.com>
2021-08-12 14:20:44 +0700

    * add file index

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { AlphabetList } from "./src/components/AlphabetList";
+export { AlphabetListProps, IData } from "./src/components/AlphabetList/types";
+export { DEFAULT_CHAR_INDEX } from "./src/values/consts"
